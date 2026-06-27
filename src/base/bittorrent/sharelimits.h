5c959c99bdebab87c5ff15b9c4294110e4055e7d
V0latyle <92863946+V0latyle@users.noreply.github.com>
2026-06-27 14:13:59 -0500

    Update sharelimits.h

--- src/base/bittorrent/sharelimits.h
+++ src/base/bittorrent/sharelimits.h
@@ -1,4 +1,8 @@
-/ * This program is free software; you can redistribute it and/or
+/*
+ * Bittorrent Client using Qt and libtorrent.
+ * Copyright (C) 2015-2026  Vladimir Golovnev <glassez@yandex.ru>
+ * Copyright (C) 2006  Christophe Dumez <chris@qbittorrent.org>
+ * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
