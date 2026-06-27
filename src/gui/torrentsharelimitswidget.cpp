aa4e65f1835db1a074894b4d578c89345a37f611
V0latyle <92863946+V0latyle@users.noreply.github.com>
2026-06-27 15:05:22 -0500

    Update torrentsharelimitswidget.cpp

--- src/gui/torrentsharelimitswidget.cpp
+++ src/gui/torrentsharelimitswidget.cpp
@@ -82,7 +82,7 @@ namespace
 
         return {};
     }
-
+}
 TorrentShareLimitsWidget::TorrentShareLimitsWidget(QWidget *parent)
     : QWidget(parent)
     , m_ui {new Ui::TorrentShareLimitsWidget}
