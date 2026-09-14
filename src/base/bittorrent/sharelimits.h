0f75a58cb831cbce3d971e600a3a8968cb46149a
V0latyle <92863946+V0latyle@users.noreply.github.com>
2026-09-13 21:53:55 -0500

    Fix missing comma

--- src/base/bittorrent/sharelimits.h
+++ src/base/bittorrent/sharelimits.h
@@ -65,7 +65,7 @@ namespace BitTorrent
             Default = -1, // special value
 
             MatchAny = 0,
-            MatchAll = 1
+            MatchAll = 1,
             Layered = 2
         };
 
